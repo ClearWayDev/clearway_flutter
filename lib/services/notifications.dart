97264767194d09e73026f28765e971632c17f8e0
usbzonelk <73486769+usbzonelk@users.noreply.github.com>
2025-04-27 10:44:22 +0000

    Added notification management service

---
+++ lib/services/notifications.dart
@@ -0,0 +1,58 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:rxdart/rxdart.dart';
+import '../firebase/firebase_options.dart';
+
+final _messageStreamController = BehaviorSubject<RemoteMessage>();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
+
+class NotificationService {
+  static final NotificationService _instance = NotificationService._internal();
+
+  factory NotificationService() => _instance;
+
+  NotificationService._internal();
+
+  Future<void> initialize() async {
+    await FirebaseMessaging.instance.requestPermission();
+
+    final messaging = FirebaseMessaging.instance;
+
+    final settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    // Listen for foreground messages
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Handling a foreground message: ${message.messageId}');
+      print('Message data: ${message.data}');
+      print('Message notification: ${message.notification?.title}');
+      print('Message notification: ${message.notification?.body}');
+
+      _messageStreamController.sink.add(message);
+    });
+
+    // Set the background message handler
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  }
+
+  Future<String?> getFCMToken() async {
+    initialize();
+    return await FirebaseMessaging.instance.getToken();
+  }
+
+  BehaviorSubject<RemoteMessage> get messageStream => _messageStreamController;
+
+  void dispose() {
+    _messageStreamController.close();
+  }
+}
